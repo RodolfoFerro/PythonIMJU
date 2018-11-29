1a4a5f1d175fddd9b049e0e3ab3147b8e391ba0f
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2018-11-28 18:41:15 -0600

    Uploaded chistes.

---
+++ chistes.py
@@ -0,0 +1,53 @@
+# Autor: Rodolfo Ferro
+# Contacto: ferro@cimat.mx
+
+from time import sleep
+
+# segundos = 5
+# print("Incia el conteo...")
+# sleep(segundos)
+# print("Woow, han pasado", segundos, "segundos.")
+
+# Saludo:
+print("Hola a todos!")
+print("Les voy a contar unos chistes!")
+print()
+sleep(2)
+
+# Chiste 1:
+print("– No cabia duda...", end='')
+input()
+print("– Y duda se fue en otro coche...")
+print("BA-DUM-TSS!")
+print()
+sleep(3)
+
+# Chiste 2:
+print("– A que te dedicas?", end='')
+input()
+print("– A mover vacas...", end='')
+input()
+print("– Eres ganadero?", end='')
+input()
+print("– No, soy maestro de zumba xdxdxd")
+print("BA-DUM-TSS!")
+print()
+sleep(3)
+
+# Chiste 3:
+print("– Te puedo robar un beso?", end='')
+input()
+print("– Con esa cara, mejor robame el celular...")
+print("BA-DUM-TSS!")
+print()
+sleep(3)
+
+# Chiste 4:
+print("– Amor, te gusta mi disfraz?", end='')
+input()
+print("– Si amor, te ves bonita de vaca.", end='')
+input()
+print("– PERO SOY UN DALMATA!")
+print("BA-DUM-TSS!")
+print()
+sleep(3)
